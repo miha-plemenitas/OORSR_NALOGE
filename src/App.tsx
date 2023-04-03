ff66f076c8b565336b83301be6b631a55d777e3c
miha_plemenitas <miha.plemenitas@student.um.si>
2023-04-03 22:02:35 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Menu} from "./Components";
+import {Telo} from "./Components";
+import {Noga} from "./Components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu teamName={"MAJKA F.C."}/>
+      <Telo/>
+      <Noga izpis={"MIHAJLOOOOOOOOOOOOOOO!!!!! NOGA"}/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,70 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    useParams,
+} from 'react-router-dom';
+import { ekipe, EkipaProps } from './Components/SeznamEkip';
+import {Igralec} from "./Components/Igralec";
+import {Info} from "./Components/Info";
+import {Opozorilo} from "./Components/Opozorilo";
+
+const Ekipa = () => {
+    const { ekipaId } = useParams();
+
+    //@ts-ignore
+    const ekipa: EkipaProps = ekipe.find((e) => e.id === parseInt(ekipaId));
+
+    return (
+        <div>
+            <h2>IME EKIPE: {ekipa.ime}</h2>
+            <h2>LETO USTANOVITVE: {ekipa.letoUstanovitve}</h2>
+            <h2>DIREKTOR: {ekipa.direktor}</h2>
+            <h2>TRENER: {ekipa.trener}</h2>
+            <ul>
+                {ekipa.igralci.map((igralec) => (
+                    <li key={igralec.ime}>
+                        <Igralec
+                            ime={igralec.ime}
+                            starost={igralec.starost}
+                            stDresa={igralec.stDresa}
+                            pozicija={igralec.pozicija}
+                            poskodovan={igralec.poskodovan}
+                        />
+                    </li>
+                ))}
+            </ul>
+            <Info stIgralcev={ekipa.igralci.length}/>
+            <Opozorilo stIgralcev={ekipa.igralci.length}/>
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/ekipa/:ekipaId',
+                element: <Ekipa />,
+            },
+        ],
+    },
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
